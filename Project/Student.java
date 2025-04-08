4802c8e24e50f8aa165b967856fdb9cdec9a6961
R-Denzel <robertdenzel5@gmail.com>
2025-04-08 16:21:00 +0300

    Students Class

--- Project/Student.java
+++ Project/Student.java
@@ -1,14 +1,13 @@
-package Project;
+package TermProject;
 
 public class Student {
-        public String name;
-        public String id;
-        public int[] scores;
+    public String name;
+    public String id;
+    public int[] scores;
 
-        public Student(String name, String id, int numSubjects) {
-            this.name = name;
-            this.id = id;
-            this.scores = new int[numSubjects];
-        }
-
+    public Student(String name, String id, int numSubjects) {
+        this.name = name;
+        this.id = id;
+        this.scores = new int[numSubjects];
+    }
 }
